0bf6dfa340a89525b559caecc1e02ce7f9148fdf
VINEET IYER <vineetice118@gst.sies.edu.in>
2021-05-24 12:58:46 +0530

    Add files via upload

---
+++ CODECHEF/DSA LEARNING SERIES/Buy me please.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 12:29:16 2021
+
+@author: Vineet
+"""
+
+try:
+    a,b,x,y=map(int,input().split())
+    sum1=a*x +b*y
+    print(sum1)
+except:
+    pass
