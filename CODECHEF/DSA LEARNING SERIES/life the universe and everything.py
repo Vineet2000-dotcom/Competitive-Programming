4f466468b26062ae3df3a65044c36d376ebc4f1a
VINEET IYER <vineetice118@gst.sies.edu.in>
2021-06-01 17:26:10 +0530

    Add files via upload

---
+++ CODECHEF/DSA LEARNING SERIES/life the universe and everything.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 17:25:13 2021
+
+@author: Vineet
+"""
+
+
+while(1):
+    n=int(input())
+
+    if(n==42):
+        break
+        
+    else:
+        print(n)
