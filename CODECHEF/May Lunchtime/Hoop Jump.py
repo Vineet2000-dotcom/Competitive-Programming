d1f7ac09390b132e20995a04e959dbea9529588d
VINEET IYER <vineetice118@gst.sies.edu.in>
2021-06-01 17:06:22 +0530

    Add files via upload

---
+++ CODECHEF/May Lunchtime/Hoop Jump.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 17:05:53 2021
+
+@author: Vineet
+"""
+
+
+t=int(input())
+while(t!=0):
+    n=int(input())
+    a=n*(n+1)/2
+    b=int(a/n)
+    print(b)
+    t=t-1
