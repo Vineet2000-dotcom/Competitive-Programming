330688b1ccdc6b470b0f5fcc3cdc889183b0abf2
VINEET IYER <vineetice118@gst.sies.edu.in>
2021-07-09 10:49:39 +0530

    Add files via upload

---
+++ Leetcode/To lower case.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 10:49:07 2021
+
+@author: Vineet
+"""
+
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        a=s.lower()
+        return a
